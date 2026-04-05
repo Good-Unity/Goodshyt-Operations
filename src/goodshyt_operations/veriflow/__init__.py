d2759d23c300b7c66f772db52f273e4f293a7652
GoodShyt Group <271941481+GoodshytGroup@users.noreply.github.com>
2026-04-05 14:28:23 -0700

    feat: add Veriflow package exports

---
+++ src/goodshyt_operations/veriflow/__init__.py
@@ -0,0 +1,17 @@
+from .models import (
+    LinearModelParameters,
+    ProblemSpec,
+    ToolTrace,
+    ValidationResult,
+    VeriflowResponse,
+)
+from .service import VeriflowService
+
+__all__ = [
+    "LinearModelParameters",
+    "ProblemSpec",
+    "ToolTrace",
+    "ValidationResult",
+    "VeriflowResponse",
+    "VeriflowService",
+]
